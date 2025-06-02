05d49806edef443c8c4d34f6bb047cb9b3146aa2
david <davidfranciscotoro@gmail.com>
2025-06-01 21:22:21 -0500

    .

--- GestorTutorias/GestorTutorias/settings.py
+++ GestorTutorias/GestorTutorias/settings.py
@@ -132,3 +132,20 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'Sistema.usuario'
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [ 'Sistema/templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
--- GestorTutorias/Sistema/views.py
+++ GestorTutorias/Sistema/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 
+
+from .models import usuario, tutor, alumno, cordinador, horario, solicitud, Feedback
+from .forms import CrearUsusuarioForm, EliminarUsuarioForm, EditarUsuarioForm
 # Create your views here.
+class IndexView:
+    def get(self, request):
+        return render(request, 'index.html')
+    
+class inicioSesion(LoginView):
+    template_name = 'login.html'  # tu plantilla de inicio de sesión
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('home')  # reemplaza 'home' por tu vista destino
+
+    def get_success_url(self):
+        return self.success_url
